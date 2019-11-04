1cfd4a32b8a2fe771b5f918a5d9eda724922d2f3
Colette Anselme <coletteanselme@live.fr>
2019-11-04 17:56:53 +0100

    Change code into class and method in program and add documentation lines

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ The intention behind Pur Beurre request is to create a program that interacts wi
 ### Approach
 - Installation of MySQL version 8.0 via the command prompt.
 - Configuration of the software under Windows 10 and connexion to MySQL (host, username and password).
+- Installing Python Modules:
+    - pip install mysql-connector
+    - pip install requests
+- Creation of the data base named "__pur_beurre__".
+- Create a class named "__Category__" in data base. This class has two attributes:
+    - __id_category__, which corresponds to the index of each category of products. This attribute allows an exclusive number to be generated automatically everytime a new record is inserted into the table.
+    - __name__, which simply corresponds to the name of each category of products.
--- program.py
+++ program.py
@@ -2,38 +2,52 @@
 
 import mysql.connector
 import requests
-# import json
 
-# Recover data from Open Food Facts URL API with request get.
-response = requests.get('https://fr.openfoodfacts.org/categories.json')
-# Save the json data in a variable.
-json_data_file = response.json()
-# Get only the data needed: data besides the one in the "tags" list are irrelevant here.
-data_tags = json_data_file.get('tags')
-# Select only the name of each category (no need for other data).
-category_data = [element.get('name', 'None') for element in data_tags]
-print(category_data)
+class OpenFoodFactsApi:
+    """docstring for OpenFoodFactsApi."""
 
-# Connection to databse pur_beurre with parameters
-my_data_base = mysql.connector.connect(
-  host="localhost",
-  user="colette",
-  passwd="",
-  database="pur_beurre"
-)
+    def __init__(self):
+        # Connection to databse pur_beurre with parameters
+        self.my_data_base = mysql.connector.connect(host="localhost",
+                            user="colette", passwd="", database="pur_beurre")
 
-#  Cursor: allows Python code to execute MySQL command in a database session.
-mycursor = my_data_base.cursor()
+    def get_categories_data(self):
+        # Recover data from Open Food Facts URL API with request get.
+        response = requests.get('https://fr.openfoodfacts.org/categories.json')
+        # Save the json data in a variable.
+        json_data_file = response.json()
+        # Get only the data needed: data besides the one in the "tags" list are irrelevant here.
+        data_tags = json_data_file.get('tags')
+        # Select only the name of each category (no need for other data).
+        category_data = [(element.get('name', 'None'),) for element in data_tags if "🍩" not in element.get('name', 'None')]
+        print(category_data)
 
-# Execute: insert a recording the table category with request SQL INSERT INTO, from a list.
-reference = (category_data)
-mycursor.execute("INSERT INTO category (name) VALUES (%s)", reference)
+        #  Cursor: allows Python code to execute MySQL command in a database session.
+        self.mycursor = self.my_data_base.cursor()
 
-# Commit: save data in data base table before closing connection.
-my_data_base.commit()
+        # Execute: insert a recording the table category with request SQL INSERT INTO, from a list.
+        self.mycursor.executemany("INSERT INTO category (name) VALUES (%s)", category_data)
 
-# Rowcount: indicates number of rows affected.
-# print(mycursor.rowcount, "was inserted.")
+        # Commit: save data in data base table before closing connection.
+        self.my_data_base.commit()
 
-# Closing the connection
-# my_data_base.close()
+        # Rowcount: indicates number of rows affected.
+        print(self.mycursor.rowcount, "was inserted.")
+
+        # Closing the connection
+        self.my_data_base.close()
+
+    def get_products_data(self, arg):
+        pass
+
+    def get_shop_data(self, arg):
+        pass
+
+    def save_result(self, arg):
+        pass
+
+    def creation_data_base_sql(self, arg):
+        pass
+
+pur_beurre_db = OpenFoodFactsApi()
+pur_beurre_db.get_categories_data()
