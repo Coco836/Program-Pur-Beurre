bba5027b4a364688e9d13ad2683200ac695721a2
Colette Anselme <coletteanselme@live.fr>
2019-11-18 19:26:03 +0100

    Modification of the structure of the program, creation of several classes, add to readme

--- README.md
+++ README.md
@@ -9,7 +9,22 @@ The intention behind Pur Beurre request is to create a program that interacts wi
 - Installing Python Modules:
     - pip install mysql-connector
     - pip install requests
+
+#### Data Base
 - Creation of the data base named "__pur_beurre__".
-- Create a class named "__Category__" in data base. This class has two attributes:
+- Create a table named "__Category__" in data base. This table has two attributes:
     - __id__, which corresponds to the index of each category of products. This attribute allows an exclusive number to be generated automatically everytime a new record is inserted into the table.
     - __name__, which simply corresponds to the name of each category of products.
+- Create a table named "__Product__" in data base. This table has several attributes:
+    - __id__, which corresponds to the the index of each product. The index is generated automatically.
+    - __name__, which corresponds to the name of the products.
+    - __description__, which corresponds to the products' ingredients.
+    - __nutrition_grade__, which corresponds to the nutriscore of a product.
+    - __id_category__, which refers to the id of the category a product is in.
+    - __id_substitute__, ???
+
+#### Python program
+- Creation of a class named "__OpenFoodFactsApi__", which retrieves API data via a "json" address.
+    - Method "__fetch_categories_data_api__" allows the program to retrieves the different informations of the categories. This method filter the informations needed for the program and take out only the name of the categories and save them in a variable.
+    - Method "__fetch_products_data_api__" allows the program to retrieves the different informations of the products in categories that were previously chosen. This method start by using the result of the previous method, then with some concatenation one is able to retrieves the json data of each categories. It returns a list of dictionnaries and each of them represent the data of one category. Each keys of the different dictionnaries have a list for value.
+- Creation of a class named "__Category__", ...
--- program.py
+++ program.py
@@ -7,12 +7,9 @@ class OpenFoodFactsApi:
     """docstring for OpenFoodFactsApi."""
 
     def __init__(self):
+        self.pur_beurre_db = DataBase()
 
-        # Connection to databse pur_beurre with parameters
-        self.my_data_base = mysql.connector.connect(host="localhost",
-                            user="colette", passwd="", database="pur_beurre")
-
-    def get_categories_data(self):
+    def fetch_categories_data_api(self):
         # Recover data from Open Food Facts URL API with request get.
         response = requests.get('https://fr.openfoodfacts.org/categories.json')
         # Save the json data in a variable.
@@ -20,43 +17,96 @@ class OpenFoodFactsApi:
         # Get only the data needed: data besides the one in the "tags" list are irrelevant here.
         data_tags = json_data_file.get('tags')
         # Select only the name of each category (no need for other data).
-        category_data = [element.get('name', 'None') for element in data_tags if "🍩" not in element.get('name', 'None')]
+        category_data = [
+                        element.get('name', 'None') for element in data_tags
+                        if "🍩" not in element.get('name', 'None')
+                        ]
+        return category_data
+
+    def fetch_products_data_api(self):
+        category_data = self.fetch_categories_data_api()
+
+        url_categories = []
+        list_product_elements = []
+        for name_cat in range(0, 20):
+            url_categories.append(f'https://fr.openfoodfacts.org/categorie/{category_data[name_cat]}.json')
+            response = requests.get(url_categories[name_cat])
+            json_data_file = response.json()
+            # Get only the data needed: data besides the one in the "tags" list are irrelevant here.
+            data_products = json_data_file.get('products')
+            product_informations = ['product_name', 'ingredients_text_debug', 'url', 'nutrition_grade_fr']
+            product_elements = {}
+            for i in product_informations:
+                get_product_elements = [element.get(i, 'None') for element in data_products]
+                product_elements.update({i : get_product_elements})
+            list_product_elements.append(product_elements)
+        return list_product_elements
+
+
+class Category:
+    """docstring for Category."""
+
+    def __init__(self):
+        self.api = OpenFoodFactsApi()
+        self.pur_beurre_db = DataBase()
+
+    def get_categories(self):
+        category_data = self.api.fetch_categories_data_api()
 
         limit_nb_category = 0
         while limit_nb_category < 20:
             sql = "INSERT INTO category (name) VALUES (%s)"
             values = [category_data[limit_nb_category]]
-            self.insert_into_database(sql, values)
+            self.pur_beurre_db.insert_into_database(sql, values)
             limit_nb_category += 1
-        # Closing the connection
-        self.my_data_base.close()
+        self.pur_beurre_db.close_database()
+
+    def save(self):
+        pass
 
 
-    def get_products_data(self):
-        # Recover data from Open Food Facts URL API with request get.
-        response = requests.get('https://fr.openfoodfacts.org/categories.json')
-        # Save the json data in a variable.
-        json_data_file = response.json()
-        # Get only the data needed: data besides the one in the "tags" list are irrelevant here.
-        data_tags = json_data_file.get('tags')
-        category_data = [element.get('url', 'None') for element in data_tags]
+class Product:
+    """docstring for Product."""
 
-        cat = []
-        for a in range(0, 19):
-            cat.append(category_data[a] + '.json')
-            resp = requests.get(cat[a])
-            js = resp.json()
-            # Get only the data needed: data besides the one in the "tags" list are irrelevant here.
-            data_products = js.get('products')
-            list_product_elements = []
-            product_elements = {}
-            product_informations = ['product_name', 'ingredients_text_debug', 'url', 'nutrition_grade_fr']
-            for i in product_informations:
-                get_product_elements = [element.get(i, 'None') for element in data_products]
-                product_elements.update({i : get_product_elements})
-            list_product_elements.append(product_elements)
-            print(list_product_elements)
+    def __init__(self):
+        self.api = OpenFoodFactsApi()
+        self.pur_beurre_db = DataBase()
 
+    def get_products(self):
+        list_product_elements = self.api.fetch_products_data_api()
+        sql = "INSERT INTO product (name, description, url, nutrition_grade, id_category) VALUES (%s, %s, %s, %s, %s)"
+        id_category = 0
+        for dict in range(0, 20):
+            id_category +=1
+            for prod in range(0, 20):
+                values = [
+                list_product_elements[dict]['product_name'][prod],
+                list_product_elements[dict]['ingredients_text_debug'][prod],
+                list_product_elements[dict]['url'][prod],
+                list_product_elements[dict]['nutrition_grade_fr'][prod],
+                id_category
+                ]
+                print(values)
+                self.pur_beurre_db.insert_into_database(sql, values)
+        self.pur_beurre_db.close_database()
+
+
+    def save(self):
+        pass
+
+
+class DataBase:
+    """docstring for DataBase."""
+
+    def __init__(self):
+        # Connection to database pur_beurre with parameters
+        self.my_data_base = mysql.connector.connect(host="localhost",
+                            user="colette", passwd="", database="pur_beurre")
+
+
+    def create_database_sql(self):
+        "Script SQL"
+        pass
 
     def insert_into_database(self, sql, values):
         #  Cursor: allows Python code to execute MySQL command in a database session.
@@ -68,14 +118,28 @@ class OpenFoodFactsApi:
         # Rowcount: indicates number of rows affected.
         print(self.mycursor.rowcount, "was inserted.")
 
+    def close_database(self):
+        # Closing the connection
+        self.my_data_base.close()
 
 
-    def get_shop_data(self, arg):
+class Shop:
+    """docstring for Shop."""
+
+    def __init__(self):
+        self.api = OpenFoodFactsApi()
+
+    def get_shop_data(self):
         pass
 
-    # def creation_data_base_sql(self, arg):
-    #     pass
+    def save(self):
+        pass
 
-pur_beurre_db = OpenFoodFactsApi()
-# pur_beurre_db.get_categories_data()
-pur_beurre_db.get_products_data()
+api = OpenFoodFactsApi()
+category = Category()
+product = Product()
+shop = Shop()
+pur_beurre_db = DataBase()
+# api.fetch_products_data_api()
+category.get_categories()
+product.get_products()
