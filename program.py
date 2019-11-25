65fad813a5ba5f86818a62d35e39dfc2d04bc864
Colette Anselme <coletteanselme@live.fr>
2019-11-25 18:12:21 +0100

    New method to add products via database instead of api

--- program.py
+++ program.py
@@ -7,7 +7,7 @@ class OpenFoodFactsApi:
     """docstring for OpenFoodFactsApi."""
 
     def __init__(self):
-        self.pur_beurre_db = DataBase()
+        pass
 
     def fetch_categories_data_api(self):
         # Recover data from Open Food Facts URL API with request get.
@@ -23,43 +23,32 @@ class OpenFoodFactsApi:
                         ]
         return category_data
 
-    def fetch_products_data_api(self):
-        category_data = self.fetch_categories_data_api()
-
-        url_categories = []
-        list_product_elements = []
-        for name_cat in range(0, 20):
-            url_categories.append(f'https://fr.openfoodfacts.org/categorie/{category_data[name_cat]}.json')
-            response = requests.get(url_categories[name_cat])
-            json_data_file = response.json()
-            # Get only the data needed: data besides the one in the "tags" list are irrelevant here.
-            data_products = json_data_file.get('products')
-            product_informations = ['product_name', 'ingredients_text_debug', 'url', 'nutrition_grade_fr']
-            product_elements = {}
-            for i in product_informations:
-                get_product_elements = [element.get(i, 'None') for element in data_products]
-                product_elements.update({i : get_product_elements})
-            list_product_elements.append(product_elements)
-        return list_product_elements
+    def fetch_products_data_api(self, myresult):
+        id_category = myresult[0]
+        category_name = myresult[1]
+        url_categories = (f'https://fr.openfoodfacts.org/categorie/{category_name}.json')
+        response = requests.get(url_categories)
+        json_data_file = response.json()
+        data_products = json_data_file.get('products')
+        return data_products, id_category, category_name
 
 
 class Category:
     """docstring for Category."""
 
-    def __init__(self):
-        self.api = OpenFoodFactsApi()
-        self.pur_beurre_db = DataBase()
+    def __init__(self, id = None, name = ''):
+        self.id = id
+        self.name = name
 
-    def get_categories(self):
-        category_data = self.api.fetch_categories_data_api()
 
-        limit_nb_category = 0
-        while limit_nb_category < 20:
+    @classmethod
+    def get_categories(cls, api, database):
+        category_data = api.fetch_categories_data_api()
+
+        for name in category_data[:20]:
             sql = "INSERT INTO category (name) VALUES (%s)"
-            values = [category_data[limit_nb_category]]
-            self.pur_beurre_db.insert_into_database(sql, values)
-            limit_nb_category += 1
-        self.pur_beurre_db.close_database()
+            values = [name]
+            database.execute(sql, values)
 
     def save(self):
         pass
@@ -68,27 +57,30 @@ class Category:
 class Product:
     """docstring for Product."""
 
-    def __init__(self):
-        self.api = OpenFoodFactsApi()
-        self.pur_beurre_db = DataBase()
+    def __init__(self, id = None, name = '', description = '', shops = [],
+                    substitute = '', categories = [], nutrition_grade = '',
+                    url = ''):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.shops = shops
+        self.substitute = substitute
+        self.categories = categories
+        self.nutrition_grade = nutrition_grade
 
-    def get_products(self):
-        list_product_elements = self.api.fetch_products_data_api()
-        sql = "INSERT INTO product (name, description, url, nutrition_grade, id_category) VALUES (%s, %s, %s, %s, %s)"
-        id_category = 0
-        for dict in range(0, 20):
-            id_category +=1
-            for prod in range(0, 20):
-                values = [
-                list_product_elements[dict]['product_name'][prod],
-                list_product_elements[dict]['ingredients_text_debug'][prod],
-                list_product_elements[dict]['url'][prod],
-                list_product_elements[dict]['nutrition_grade_fr'][prod],
-                id_category
-                ]
-                print(values)
-                self.pur_beurre_db.insert_into_database(sql, values)
-        self.pur_beurre_db.close_database()
+    @classmethod
+    def get_products(cls, database, api):
+        for id in range(1, 21):
+            (data_products, id_category, category_name) = database.select_category(api, id)
+            for data in range(0, 20):
+                prod_name = [element.get('product_name') for element in data_products]
+                prod_ingredients = [element.get('ingredients_text_fr') for element in data_products]
+                prod_url = [element.get('url') for element in data_products]
+                prod_nutriscore = [element.get('nutrition_grade_fr') for element in data_products]
+                prod_data = [prod_name[data], prod_ingredients[data], prod_url[data], prod_nutriscore[data], id_category]
+                sql = "INSERT INTO product (name, description, url, nutrition_grade, id_category) VALUES (%s, %s, %s, %s, %s)"
+                values = prod_data
+                database.execute(sql, values)
 
 
     def save(self):
@@ -101,22 +93,29 @@ class DataBase:
     def __init__(self):
         # Connection to database pur_beurre with parameters
         self.my_data_base = mysql.connector.connect(host="localhost",
-                            user="colette", passwd="", database="pur_beurre")
+                            user="colette", passwd="", database="pur_beurre", buffered = True)
+        #  Cursor: allows Python code to execute MySQL command in a database session.
+        self.mycursor = self.my_data_base.cursor()
 
 
     def create_database_sql(self):
         "Script SQL"
         pass
 
-    def insert_into_database(self, sql, values):
-        #  Cursor: allows Python code to execute MySQL command in a database session.
-        self.mycursor = self.my_data_base.cursor()
+    def execute(self, sql, values):
         # Execute: insert a recording the table category with request SQL INSERT INTO, from a list.
         self.mycursor.execute(sql, values)
         # Commit: save data in data base table before closing connection.
         self.my_data_base.commit()
         # Rowcount: indicates number of rows affected.
         print(self.mycursor.rowcount, "was inserted.")
+
+    def select_category(self, api, id):
+        id = id
+        self.mycursor.execute("SELECT id, name FROM Category WHERE id = '%s'" % (id))
+        myresult = self.mycursor.fetchone()
+        (data_products, id_category, category_name) = api.fetch_products_data_api(myresult)
+        return data_products, id_category, category_name
 
     def close_database(self):
         # Closing the connection
@@ -126,20 +125,24 @@ class DataBase:
 class Shop:
     """docstring for Shop."""
 
-    def __init__(self):
-        self.api = OpenFoodFactsApi()
+    def __init__(self, id = None, name = ''):
+        self.id = id
+        self.name = name
 
-    def get_shop_data(self):
+    # @classmethod
+    def get_shop_data(cls, api, database):
         pass
 
     def save(self):
         pass
 
 api = OpenFoodFactsApi()
+database = DataBase()
 category = Category()
 product = Product()
-shop = Shop()
-pur_beurre_db = DataBase()
+# shop = Shop()
+# shop = get_shop_data(api, database)
 # api.fetch_products_data_api()
-category.get_categories()
-product.get_products()
+category.get_categories(api, database)
+product.get_products(database, api)
+# category.retrieve_category_data(database)
