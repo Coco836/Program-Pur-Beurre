701fbda8ffc61c7be559ef4ae208835c70b6d088
Colette Anselme <coletteanselme@live.fr>
2019-12-02 19:56:24 +0100

    Add Link_prod_cat

--- program.py
+++ program.py
@@ -23,14 +23,12 @@ class OpenFoodFactsApi:
                         ]
         return category_data
 
-    def fetch_products_data_api(self, myresult):
-        id_category = myresult[0]
-        category_name = myresult[1]
-        url_categories = (f'https://fr.openfoodfacts.org/categorie/{category_name}.json')
+    def fetch_products_data_api(self, category: "Category"):
+        url_categories = (f'https://fr.openfoodfacts.org/categorie/{category.name}.json')
         response = requests.get(url_categories)
         json_data_file = response.json()
         data_products = json_data_file.get('products')
-        return data_products, id_category, category_name
+        return data_products
 
 
 class Category:
@@ -57,31 +55,75 @@ class Category:
 class Product:
     """docstring for Product."""
 
-    def __init__(self, id = None, name = '', description = '', shops = [],
-                    substitute = '', categories = [], nutrition_grade = '',
-                    url = ''):
+    def __init__(self, id = None, name = '', description = '',
+                     url = '', nutrition_grade = '', substitute = ''):
         self.id = id
         self.name = name
         self.description = description
-        self.shops = shops
-        self.substitute = substitute
-        self.categories = categories
+        self.url = url
         self.nutrition_grade = nutrition_grade
+        self.substitute = substitute
 
     @classmethod
-    def get_products(cls, database, api):
-        for id in range(1, 21):
-            (data_products, id_category, category_name) = database.select_category(api, id)
-            for data in range(0, 20):
-                prod_name = [element.get('product_name') for element in data_products]
-                prod_ingredients = [element.get('ingredients_text_fr') for element in data_products]
-                prod_url = [element.get('url') for element in data_products]
-                prod_nutriscore = [element.get('nutrition_grade_fr') for element in data_products]
-                prod_data = [prod_name[data], prod_ingredients[data], prod_url[data], prod_nutriscore[data], id_category]
-                sql = "INSERT INTO product (name, description, url, nutrition_grade, id_category) VALUES (%s, %s, %s, %s, %s)"
-                values = prod_data
+    def get_products(cls, api, database):
+        categories = database.select_categories()
+
+        for category in categories:
+            data_products = api.fetch_products_data_api(category)
+            for data_product in data_products:
+                prod_name = data_product.get('product_name', 'None')
+                prod_ingredients = data_product.get('ingredients_text_fr')
+                prod_url = data_product.get('url')
+                prod_nutriscore = data_product.get('nutrition_grade_fr')
+                prod_categories = data_product.get('categories')
+
+                values = [
+                    prod_name, prod_ingredients, prod_url, prod_nutriscore,
+                ]
+                sql = (
+                    "INSERT INTO product (name, description, url, "
+                    "nutrition_grade) VALUES (%s, %s, %s, %s)"
+                )
                 database.execute(sql, values)
 
+                last_id_product = database.mycursor.lastrowid
+                last_id_cat = category.id
+
+                product.link_prod_cat(database, prod_categories, last_id_product, last_id_cat)
+
+
+    def link_prod_cat(self, database, prod_categories, last_id_product, last_id_cat):
+        list_of_product_categories = prod_categories.split(',')
+
+        for item in list_of_product_categories:
+            list_of_product_categories = [item.strip() for item in list_of_product_categories]
+
+        for prod_category in list_of_product_categories:
+            database.mycursor.execute("SELECT id FROM Category WHERE name = %s", (prod_category,))
+            myresult = database.mycursor.fetchone()
+            sql = "INSERT IGNORE INTO link_prod_cat (id_category, id_product) VALUES (%s, %s)"
+
+            if myresult != None:
+                values = [myresult[0], last_id_product]
+                database.execute(sql, values)
+            else:
+                values = [last_id_cat, last_id_product]
+                database.execute(sql, values)
+
+
+    def get_substitutes(self, database):
+        database.alter_table_product()
+        products = database.select_products()
+        for product in products:
+            # if product.nutrition_grade != "a":
+            #     database.mycursor.execute(
+            #                             "SELECT id_product FROM Link_prod_cat "
+            #                             "INNER JOIN Product "
+            #                             "ON Product.id = Link_prod_cat.id_product "
+            #                             "WHERE Link_prod_cat.id_product = %s", (product.id,)
+            #                             )
+            #     myresult = database.mycursor.fetchone()
+            #     print(product.name, myresult)
 
     def save(self):
         pass
@@ -97,7 +139,6 @@ class DataBase:
         #  Cursor: allows Python code to execute MySQL command in a database session.
         self.mycursor = self.my_data_base.cursor()
 
-
     def create_database_sql(self):
         "Script SQL"
         pass
@@ -110,12 +151,21 @@ class DataBase:
         # Rowcount: indicates number of rows affected.
         print(self.mycursor.rowcount, "was inserted.")
 
-    def select_category(self, api, id):
-        id = id
-        self.mycursor.execute("SELECT id, name FROM Category WHERE id = '%s'" % (id))
-        myresult = self.mycursor.fetchone()
-        (data_products, id_category, category_name) = api.fetch_products_data_api(myresult)
-        return data_products, id_category, category_name
+    def select_categories(self, limit = 20)-> list:
+        self.mycursor.execute(f"SELECT id, name FROM Category limit {limit}")
+        return [Category(id, name) for id, name in self.mycursor.fetchall()]
+
+    def alter_table_product(self):
+        self.mycursor.execute("ALTER TABLE Product ADD id_substitute SMALLINT UNSIGNED")
+
+    def select_products(self):
+        self.mycursor.execute(f"SELECT id, name, description, url, nutrition_grade FROM Product")
+        return [
+                Product(id, name, description, url, nutrition_grade)
+                for id, name, description, url, nutrition_grade
+                in self.mycursor.fetchall()
+        ]
+
 
     def close_database(self):
         # Closing the connection
@@ -143,6 +193,8 @@ product = Product()
 # shop = Shop()
 # shop = get_shop_data(api, database)
 # api.fetch_products_data_api()
-category.get_categories(api, database)
-product.get_products(database, api)
+# category.get_categories(api, database)
+# product.get_products(api, database)
+# product.link_prod_cat()
+product.get_substitutes(database)
 # category.retrieve_category_data(database)
